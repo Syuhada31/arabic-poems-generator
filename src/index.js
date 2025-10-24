46b0d5dbe3e0f37f1eae775b994c2e69abdd1b3d
Syuhada J. <syuhada3137@gmail.com>
2025-10-25 04:11:15 +0800

    remove console.log

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 function displayPoem(response) {
 
-  console.log("poem generated");
+ 
   new Typewriter("#poem", {
   strings: response.data.answer,
   autoStart: true,
@@ -28,10 +28,7 @@ let poemElement = document.querySelector("#poem");
 poemElement.classList.remove("hidden");
 poemElement.innerHTML = `<div class = "generating">⏳Generating an Arabic poem about ${instructionsInput.value} </div >`;
 
-  console.log("Generating poem");
-  console.log(`Prompt: ${prompt}`);
-  console.log(`Context: ${context}`);
-  axios.get(apiUrl).then(displayPoem);
+axios.get(apiUrl).then(displayPoem);
   
   
   
